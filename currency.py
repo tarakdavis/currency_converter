9cc0d272d2691fb18f44d792243a42137af14d2d
Tara Davis <tkalindavis@gmail.com>
2016-07-19 10:28:22 -0400

    Refactored and added two part function to separate and assign the symbol.

--- currency.py
+++ currency.py
@@ -1,5 +1,5 @@
 class Currency:
-    def __init__(self, amount, currency_code):
+    def __init__(self, amount, currency_code = 'USD'):
         self.amount = amount
         self.currency_code = currency_code
 
@@ -7,20 +7,30 @@ class Currency:
         return self.amount == other.amount and self.currency_code == other.currency_code
 
     def __add__(self, other):
-        try:
-            if self.currency_code != other.currency_code:
-                raise DifferentCurrencyCodeError
-            return (self.amount + other.amount, self.currency_code)
-        else ZeroDivisionError:
-            pass
+        if self.currency_code != other.currency_code:
+            raise DifferentCurrencyCodeError
+        return (self.amount + other.amount, self.currency_code)
+
 
     def __sub__(self, other):
-            try:
-                if self.currency_code != other.currency_code:
-                    raise DifferentCurrencyCodeError
-                return (self.amount + other.amount, self.currency_code)
-            else ZeroDivisionError:
-                pass
+        if self.currency_code != other.currency_code:
+            raise DifferentCurrencyCodeError
+        return (self.amount - other.amount, self.currency_code)
+
+    def __mul__(self, amount):
+        return (self.amount * amount, self.currency_code)
+
+
+    def assign_currency_code_to_symbol(self):
+        symbols = {'$': 'USD', '€': 'EUR', '¥': 'JPY'}
+        for character in self[0]:
+            if character in symbols:
+                return symbols[character]
+
+    def separate_symbol_and_amount(self):
+        listed_amount = list(self)
+        del listed_amount[0]
+        return float(''.join(listed_amount))
 
 class DifferentCurrencyCodeError(Exception):
     pass
--- currency_test.py
+++ currency_test.py
@@ -1,5 +1,6 @@
-from currency import Currency,
-import nose.tools import assert_raises
+from currency import Currency
+from currency import DifferentCurrencyCodeError
+from nose.tools import assert_raises
 
 def test_create_currency_with_amount_and_code():
     one_dollar = Currency(1, 'USD')
@@ -41,10 +42,28 @@ def test_raise_error_when_currencys_are_added():
     curr1 = Currency(1, 'USD')
     curr2 = Currency(1, 'EUR')
 
-    assert_raises(DifferentCurrencyCodeError, Currency, __add__, curr1, curr2)
+    assert_raises(DifferentCurrencyCodeError, Currency.__add__, curr1, curr2)
 
 def test_raise_error_when_currencys_are_subtracted():
     curr1 = Currency(1, 'USD')
     curr2 = Currency(1, 'EUR')
 
-    assert_raises(DifferentCurrencyCodeError, Currency, __sub__, curr1, curr2)
+    assert_raises(DifferentCurrencyCodeError, Currency.__sub__, curr1, curr2)
+
+def test_currencys_can_be_multiplied_by_an_int():
+    curr1 = Currency(5, 'USD')
+    value = int(10)
+    assert curr1 * value == (50, 'USD')
+
+def test_currencys_can_be_multiplied_by_a_float():
+    curr1 = Currency(5, 'USD')
+    value = float(10.5)
+    assert curr1 * value == (52.5, 'USD')
+
+def test_replace_symbol_with_currency_code():
+    curr1 = '$1.20'
+    curr2 = '€1.20'
+    curr3 = '¥1.20'
+    assert Currency(Currency.separate_symbol_and_amount(curr1)), Currency(Currency.assign_currency_code_to_symbol(curr1)) == Currency(1.20, 'USD')
+    assert Currency(Currency.separate_symbol_and_amount(curr2)), Currency(Currency.assign_currency_code_to_symbol(curr2)) == Currency(1.20, 'EUR')
+    assert Currency(Currency.separate_symbol_and_amount(curr3)), Currency(Currency.assign_currency_code_to_symbol(curr3)) == Currency(1.20, 'JPY')
